bd5865f6c971103c2827ea39582a6a13d7b52952
Camila Villaverde <camilavillaverde3@gmail.com>
2022-09-09 16:53:10 -0300

    contenedor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import Products from './components/ItemListContainer';
+import Products from './containers/ItemListContainer';
 
 function App() {
   return (
