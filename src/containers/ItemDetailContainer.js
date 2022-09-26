5f09cc0baf929fec70288d8432c3e1dd329a6b80
Camila Villaverde <camilavillaverde3@gmail.com>
2022-09-26 12:19:44 -0300

    loading-itemdetail

--- src/containers/ItemDetailContainer.js
+++ src/containers/ItemDetailContainer.js
@@ -2,27 +2,37 @@ import React, { useEffect, useState } from "react"
 import ItemDetail from "../components/ItemDetail"
 import Products from "../utils/Products"
 import { useParams } from "react-router-dom"
+import Loading from "../components/Loading"
 
 const ItemDetailContainer = () => {
     const [productArray, setproductArray] = useState([])
+    const [Load, setLoad] = useState(true)
     const { id } = useParams()
 
     useEffect(() => {
         const getItem = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
+                    setLoad(true)
                     // eslint-disable-next-line
                     resolve(Products.find(item => item.id == id)) 
-                }, 2000)
+                }, 1000)
             })
         }
     
         getItem()
             .then(response => {
                 setproductArray(response)
+                setLoad(false)
             })
             .catch(() => console.log("Error"))
     })
+
+    if(Load) return (
+        <div className="Loading">
+            <Loading/> 
+        </div>
+    )
 
     return (
         <div className="ItemDetail">
