a5d74466c5220c4ae891af534dfe7f10fbdfb30c
Camila Villaverde <camilavillaverde3@gmail.com>
2022-09-30 18:12:37 -0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,8 @@
 import { Link } from "react-router-dom";
+import beauty from "../img/beauty.jpg";
+import beauty1 from "../img/beauty1.jpg"
+import product from "../img/product.jpg";
+import product1 from "../img/product1.jpg";
 
 const Home = () => {
     return (
@@ -8,6 +12,22 @@ const Home = () => {
                     <h2>Cuida tu piel</h2>
                     <Link to="/products"><button className="Shop-Now">Shop Now</button></Link>
                 </div>           
+            </section>
+
+            <section id="Main">
+                <div className="text">
+                    <p>Bienvenido a <span>Aquira</span></p>
+                </div>
+                <div className="images">
+                    <div className="images-1">
+                        <img src={product} alt="products"/>
+                        <img src={beauty} alt="girl"/>
+                    </div>
+                    <div className="images-2">
+                        <img src={beauty1} alt="girl"/>
+                        <img src={product1} alt="products"/>
+                    </div>
+                </div>
             </section>
         </>
     )
